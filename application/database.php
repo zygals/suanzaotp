9a2af1ebd9ccba6d9d9c0ab194b171d96da26421
tancky <546349822@qq.com>
2017-10-10 14:03:23 +0800

    opt

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'newsuanzaodb',
+    'database'        => 'suanzao',
     // 用户名
-    'username'        => 'newsuanzao',
+    'username'        => 'root',
     // 密码
-    'password'        => 'newsuanzao',
+    'password'        => 'root',
     // 端口
     'hostport'        => '',
     // 连接dsn
--- application/index/view/layout/base.php
+++ application/index/view/layout/base.php
@@ -8,6 +8,7 @@
     <link rel="Shortcut Icon" href="__STATIC__img/favicon.ico">
     <link rel="stylesheet" type="text/css" href="__STATIC__css/reset.css"/>
     <link rel="stylesheet" type="text/css" href="__STATIC__css/index.css"/>
+    <link rel="stylesheet" type="text/css" href="__STATIC__css/homepage.css"/>
     <link rel="stylesheet" href="__STATIC__css/swiper-3.4.2.min.css">
     <script src="__STATIC__js/jquery-2.0.3.js"></script>
     <script src="__STATIC__js/swiper-3.4.2.jquery.min.js"></script>
