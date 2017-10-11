c202b74361b13687d5f7f37284a0b66ffe9d15f1
zygals <2799024884@qq.com>
2017-10-11 10:25:35 +0800

    menu again

---
+++ menu.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 5.7.19, for Linux (x86_64)
+--
+-- Host: localhost    Database: suanzao
+-- ------------------------------------------------------
+-- Server version	5.7.19-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `menu_admin`
+--
+
+DROP TABLE IF EXISTS `menu_admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menu_admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL COMMENT '导航名称',
+  `pid` int(11) NOT NULL DEFAULT '0' COMMENT '导航上级id，总分两级,0表示一级',
+  `controller` varchar(100) DEFAULT '' COMMENT '控制器,为一级时为""',
+  `action` varchar(100) DEFAULT '' COMMENT '控制器中方法,为一级时为""',
+  `param` varchar(100) DEFAULT '' COMMENT '参数',
+  `sort` int(11) DEFAULT '0',
+  `create_time` int(11) DEFAULT '0',
+  `update_time` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COMMENT='后台左侧导航';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu_admin`
+--
+
+LOCK TABLES `menu_admin` WRITE;
+/*!40000 ALTER TABLE `menu_admin` DISABLE KEYS */;
+INSERT INTO `menu_admin` VALUES (19,'主要管理',0,'','','',0,1505358580,1505358580),(20,'资讯/案例',19,'Article','index','',0,1505358628,1506490666),(22,'广告图',19,'Ad','index','',1,1505358704,1505970654),(23,'其它管理',0,'','','',1,1505358751,1505358751),(30,'文件下载',19,'Dfile','index','',2,1505970647,1505970647),(26,'类别',23,'Cate','index','',0,1505369468,1505970973),(32,'管理员',23,'Admin','index','',0,1507605548,1507605548),(31,'友情链接',23,'Friend','index','',0,1507601734,1507601734),(29,'SEO设置',23,'SeoSet','index','',4,1505699413,1505699516);
+/*!40000 ALTER TABLE `menu_admin` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-10-11 10:24:24
